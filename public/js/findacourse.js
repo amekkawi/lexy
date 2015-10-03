04b79f5cf0636f9202f6ec74832c8c792ffa5fb6
Lexy Kruse <git@lexykruse.biz>
2015-10-03 18:51:08 -0400

    Fixed duplication by using .empty() also made course box hidden until submit successfully brings back results by adding style display: none in html, then in the js file, used .css display none. Also, since course level will only have one item, got rid of append to and just added the span to the html and used .text.

--- public/js/findacourse.js
+++ public/js/findacourse.js
@@ -4,7 +4,8 @@
 var $CourseNumber = $('#CourseNumber');
 var $CourseTitle = $('#CourseTitle');
 var $Courses = $('#Courses');
-var $Level = $('#Level');
+var $CourseBox = $('#CourseBox');
+var $Level = $('#Level > span');
 var $Staff = $('#Staff');
 var $Subjects = $('#Subjects');
 
@@ -20,19 +21,25 @@ $.ajax({
 
 		$('#FindACourse').on('submit', function(evt) {
 			evt.preventDefault();
+
+			// Hide the results before doing a new search.
+			$CourseBox.css('display', 'none');
+
 			var $UserInput = ($CourseNumber.val());
 			console.log($UserInput);
 			for (var i = 0; i < data.length; i++) {
 				//console.log(data[i].l, data[i].guid, $UserInput, $UserInput == data[i].guid, $UserInput === data[i].guid);
 				if ($UserInput == data[i].guid) {
+					$CourseBox.css('display', '');
 					console.log([i]);
 					$CourseTitle.text(data[i].l);
-					var $CourseLevel = $('<span></span>').text(data[i].level);
-					$CourseLevel.appendTo($Level);
+					$Level.text(data[i].level);
+					$Staff.empty();
 					for (var a = 0; a < data[i].staff.length; a++) {
 						var $EachStaffName = $("<li></li>").text(data[i].staff[a]);
 						$EachStaffName.appendTo($Staff);
 					}
+					$Subjects.empty();
 					for (var b = 0; b < data[i].subjects.length; b++) {
 						var $EachSubject = $("<li></li>").text(data[i].subjects[b]);
 						$EachSubject.appendTo($Subjects);
