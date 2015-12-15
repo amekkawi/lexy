53879004391e0991aa83907764c0c2d37f053921
Lexy Kruse <git@lexykruse.biz>
2015-12-15 15:42:05 -0500

    wip- incorporated whoami ajax call into studentlogin.js. student login works. Added a log in dash, logged in dash that appear/are hidden depending on if anyone is logged in. Navigation links change slightly depending on if anyone is logged on. Added window.someone is logged in. TODO: figure out where to store info, such as a guid number of a class someone signed up for with their username. (?)

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,38 @@
 var express = require('express');
 var router = express.Router();
 
+var userNames = {
+  lexy: 'geo',
+  andre: 'whiskers',
+  michael: 'fox'
+};
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
+});
+
+router.get('/newlogin', function(req, res) {
+  var newLoginName = req.query.newusername;
+  var newPass = req.query.newpassword;
+  res.json({
+    status: 'Received the new user info',
+
+
+
+
+
+
+  //{userNames[newLoginName] = [newPass];}
+
+
+
+  });
+  console.log(newLoginName, newPass);
+});
+
+router.get('/get', function(req, res) {
+  res.json(userNames);
 });
 
 module.exports = router;
