afc857c040d3d4b6a0d0d50bb4caf1f1bfa4be4d
Lexy Kruse <git@lexykruse.biz>
2015-12-21 21:25:25 -0500

    Removed a lot of user related stuff from index.js and put it in users.js. Started to work on fs.readFile and fs.writeFile for getting and saving data.

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,6 @@
 var express = require('express');
 var router = express.Router();
 
-var userNames = {
-  lexy: 'geo',
-  andre: 'whiskers',
-  michael: 'fox',
-}
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Lexy' });
@@ -24,10 +18,9 @@ router.get('/coursecatalog', function(req, res) {
 });
 
 router.get('/courseinformation/:guid', function(req, res) {
-  var guidValue = req.params.guid;
-  //the second argument below, is a plain object, and this is the way to pass data on to the template (the ejs file)
+  //the second argument below, is a plain object, and this is the way to pass data (you're telling it which json file to refer to!) on to the template (the ejs file)
   res.render('courseinformation', {
-    guid: guidValue
+    coursenumber: req.params.guid,
   });
 });
 
@@ -35,57 +28,8 @@ router.get('/studentlogin', function(req, res) {
   res.render('studentlogin');
 });
 
-router.get('/whoami', function(req, res) {
-  res.json({
-    username: req.session.loggedInUser
-  });
-});
-
-router.get('/logout', function(req, res) {
-  // Record what the username was before we clear it
-  var oldUsername = req.session.loggedInUser;
-
-  // Clear the username so they are no longer logged in
-  req.session.loggedInUser = undefined;
-
-  res.json({
-    username: oldUsername
-  });
-});
-
-router.get('/login', function(req, res) {
-  var login = req.query.username;
-  var pass = req.query.password;
-
-  if (typeof login !== 'string') {
-    res.json({
-      status: 'fail',
-      message: 'the "login" query param is required!'
-    });
-    return;
-  }
-
-  console.log(login, pass);
-  var correctPass = userNames[login];
-  console.log(correctPass);
-
-  //var usernameExists = userNames.hasOwnProperty(login);
-
-  if (typeof correctPass === 'string' && pass === correctPass) {
-    // record who is logged in for the session
-    req.session.loggedInUser = login;
-
-    res.json({
-      status: 'success'
-    });
-  }
-  else {
-    res.json({
-      status: 'fail',
-      message: ''
-    });
-  }
-
+router.get('/shoppingcart', function(req, res) {
+  res.render('shoppingcart');
 });
 
 
--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,29 @@
+var fs = require('fs');
 var express = require('express');
 var router = express.Router();
 
 var userNames = {
-  lexy: 'geo',
-  andre: 'whiskers',
-  michael: 'fox'
+  //lexy: 'geo',
+  //andre: 'whiskers',
+  //michael: 'fox'
 };
+
+// Read previously saved users and passwords
+fs.readFile('./runtime/usernames.json', {encoding: 'utf8'}, function (err, data) {
+  //if (err) throw err;
+  //console.log(data);
+
+  // if file exists, parse json and set to userNames
+  if (!err) {
+    userNames = JSON.parse(data);
+    console.log('loaded usernames');
+  }
+
+  // otherwise, console.log that we couldn't find the data
+  else {
+    console.log
+  }
+});
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -15,24 +33,87 @@ router.get('/', function(req, res, next) {
 router.get('/newlogin', function(req, res) {
   var newLoginName = req.query.newusername;
   var newPass = req.query.newpassword;
+
+  // Log the object before making the change
+  console.log('before', userNames);
+
+  // Add new username
+  userNames[newLoginName] = newPass;
+
   res.json({
-    status: 'Received the new user info',
+    status: 'Received the new user info'
+  });
 
-
-
-
-
-
-  //{userNames[newLoginName] = [newPass];}
-
-
+  fs.writeFile('./runtime/usernames.json', JSON.stringify(userNames), function(err) {
+    if (err) {
+      console.log('whoops')
+    }
 
   });
-  console.log(newLoginName, newPass);
+
+  // Log the object again to see the changes
+  console.log('after', userNames);
 });
 
 router.get('/get', function(req, res) {
   res.json(userNames);
 });
+
+router.get('/whoami', function(req, res) {
+  //setTimeout(function(){
+  res.json({
+    username: req.session.loggedInUser
+  });
+  //}, 1000);
+});
+
+router.get('/loggingout', function(req, res) {
+  // Record what the username was before we clear it
+  var oldUsername = req.session.loggedInUser;
+
+  // Clear the username so they are no longer logged in
+  req.session.loggedInUser = undefined;
+
+  res.json({
+    username: oldUsername
+  });
+});
+
+router.get('/login', function(req, res) {
+  var login = req.query.username;
+  var pass = req.query.password;
+
+  if (typeof login !== 'string') {
+    res.json({
+      status: 'fail',
+      message: 'the "login" query param is required!'
+    });
+    return;
+  }
+
+  console.log(login, pass);
+  var correctPass = userNames[login];
+  console.log(correctPass);
+
+  //var usernameExists = userNames.hasOwnProperty(login);
+
+  if (typeof correctPass === 'string' && pass === correctPass) {
+    // record who is logged in for the session
+    req.session.loggedInUser = login;
+
+    res.json({
+      status: 'loginsuccessful'
+    });
+  }
+  else {
+    res.json({
+      status: 'loginfailed',
+      message: ''
+    });
+  }
+
+});
+
+
 
 module.exports = router;
